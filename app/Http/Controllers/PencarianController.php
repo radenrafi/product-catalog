d77b5c2c926ca815398f41645b6ae77b57cb12ac
Raden Rafi <raden.rafi.personal@gmail.com>
2023-11-13 20:47:21 +0700

    fixing search feature

--- app/Http/Controllers/PencarianController.php
+++ app/Http/Controllers/PencarianController.php
@@ -10,28 +10,8 @@ class PencarianController extends Controller
 {
     public function proses(Request $request)
     {
-        $produks = Produk::where('nama', 'LIKE', '%'.$request->s.'%')->get();
-        $i = 0;
-            $temp = collect([]);
-            foreach ($produks as $produk) {
-                if ($i == 0) {
-                    $gambar = GambarProduk::where('produk_id', $produk->id)->latest()->first();
-                    if ($gambar == null) {
-                        $temp->push(['id' => $produk->id, 'nama' => $produk->nama, 'harga' => $produk->harga, 'gambar' => 'kosong.jpg']);
-                    }else{
-                        $temp->push(['id' => $produk->id, 'nama' => $produk->nama, 'harga' => $produk->harga, 'gambar' => $gambar->gambar]);
-                    }
-                    $i++;
-                }
-                else {
-                    $gambar = GambarProduk::where('produk_id', $produk->id)->latest()->first();
-                    if ($gambar == null) {
-                        $temp->push(['id' => $produk->id, 'nama' => $produk->nama, 'harga' => $produk->harga, 'gambar' => 'kosong.jpg']);
-                    }else{
-                        $temp->push(['id' => $produk->id, 'nama' => $produk->nama, 'harga' => $produk->harga, 'gambar' => $gambar->gambar]);
-                    }
-                }
-            }
-            return view('produk.indexUser', ['produks' => $temp]);
+        $produks = Produk::where('nama', 'LIKE', '%' . $request->s . '%')->paginate(10);
+
+        return view('produk.indexUser', ['produks' => $produks]);
     }
 }
