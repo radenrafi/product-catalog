1b1591fd3154dfe19cbb863e54812f216f58c704
Raden Rafi <raden.rafi.personal@gmail.com>
2023-11-15 13:41:12 +0700

    tes

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -1,26 +1,32 @@
 @extends('layouts.canvas')
 
 @section('content')
-        {{--  begin of carousel  --}}
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"  aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    {{--  begin of carousel  --}}
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        {{--  begin carousel desktop  --}}
+        <div class="carousel-inner carousel-desktop">
+            <div class="carousel-item active">
+                <img src="{{ asset('images/welcome/crsl-1.jpg') }}" class="d-block w-100" height="500"
+                    style="object-fit: cover; object-position: center" alt="...">
             </div>
-            {{--  begin carousel desktop  --}}
-            <div class="carousel-inner carousel-desktop">
-                <div class="carousel-item active">
-                  <img src="{{asset('images/welcome/crsl-1.jpg')}}" class="d-block w-100" height="500" style="object-fit: cover; object-position: center" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="{{asset('images/welcome/crsl-2.jpg')}}" class="d-block w-100" height="500" style="object-fit: cover; object-position: center"  alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="{{asset('images/welcome/crsl-3.jpg')}}" class="d-block w-100" height="500" style="object-fit: cover; object-position: center"  alt="...">
-                </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/welcome/crsl-2.jpg') }}" class="d-block w-100" height="500"
+                    style="object-fit: cover; object-position: center" alt="...">
             </div>
-            {{--  ending carousel desktop  --}}
+            <div class="carousel-item">
+                <img src="{{ asset('images/welcome/crsl-3.jpg') }}" class="d-block w-100" height="500"
+                    style="object-fit: cover; object-position: center" alt="...">
+            </div>
+        </div>
+        {{--  ending carousel desktop  --}}
 
         <div class="btn-control">
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -37,37 +43,38 @@
     </div>
     {{--  ending of caraousel  --}}
 
-        {{--  begin of kategori  --}}
-        <div class="d-flex justify-content-start text-center category">
-            <div class="button-group" role="group">
-              <figure class="figure">
+    {{--  begin of kategori  --}}
+    <div class="d-flex justify-content-start text-center category">
+        <div class="button-group" role="group">
+            <figure class="figure">
                 <a href="{{ url('/') }}" class="d-flex flex-row">
                     <p class="img-fluid">Semua</p>
                 </a>
-              </figure>
-              <figure class="figure">
+            </figure>
+            <figure class="figure">
                 <a href="{{ url('/produk/kategori/learning-media') }}" class="d-flex flex-row">
                     <p class="img-fluid">Learning Media</p>
                 </a>
-              </figure>
-              <figure class="figure">
+            </figure>
+            <figure class="figure">
                 <a href="{{ url('/produk/kategori/digital-book') }}" class="d-flex flex-row">
                     <p class="img-fluid">Digital Book</p>
                 </a>
-              </figure>
-              <figure class="figure">
+            </figure>
+            <figure class="figure">
                 <a href="{{ url('/produk/kategori/metaverse') }}" class="d-flex flex-row">
                     <p class="img-fluid">Metaverse</p>
                 </a>
-              </figure>
-            </div>
+            </figure>
         </div>
-        {{--  ending of kategori  --}}
+    </div>
+    {{--  ending of kategori  --}}
 
     {{--  Begin of product  --}}
     <div class="container product">
         <section>
             <div class="row">
+                @dump($produkAkriliks)
                 @foreach ($produkAkriliks as $produk)
                     <div class="col-6 col-lg-4 mt-3">
                         <div class="card card-product">
@@ -161,7 +168,8 @@
                                     <p class="card-price">Rp. {{ $produk->harga }}</p>
                                 </div>
                                 <div class="col d-flex justify-content-center align-items-center">
-                                    <a href="{{ url('/produk/'.$produk['id']) }}" class="btn d-flex justify-content-center align-items-center btn-product">
+                                    <a href="{{ url('/produk/' . $produk['id']) }}"
+                                        class="btn d-flex justify-content-center align-items-center btn-product">
                                         Buy
                                     </a>
                                 </div>
@@ -173,5 +181,4 @@
         </section>
     </div>
     {{--  Ending of card  --}}
-
 @endsection
